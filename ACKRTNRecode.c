fac8a43d2128d06cef4e150a351500fbe3816f9f
Mark Hutcheson <meh2481@vt.edu>
2012-02-26 22:10:39 +1400

    Initial version

---
+++ ACKRTNRecode.c
@@ -0,0 +1,427 @@
+// ACKRTNRecode.c
+// ACK-3D C source
+// Mark H's reimplementation of needed routines from ACKRTN*.ASM
+// Copyright (c) 2012, except functions based off C or ASM routines in book
+
+#include "ACKRTNRecode.h"
+
+//BuildUpView - our big workhorse to make sure the view and everything gets built properly.
+void BuildUpView(void)
+{
+    //Clear out the screen buffer to start our next pass (Just so we avoid issues with uninitialized stuff when the map isn't
+                                                       // built fully yet or something)
+    int j,k; // j/k. Seewhatididthere?
+    for(j = 0; j < 320; j++)
+    {
+        for(k = 0; k < 200; k++)
+        {
+            gScrnBuffer[j*200+k] = 0;
+        }
+    }
+
+
+    CheckDoors();   //Check the doors to update them and such.
+
+    //Clear our hit map
+    int i;  //General-purpose increment variable (don't declare in for loops because C doesn't support it)
+    for(i = 0; i < 4096; i++)
+    {
+        HitMap[i] = 0;
+    }
+
+    MaxDistance = 0; //Reset our maximum distance
+
+    FoundObjectCount = MoveObjectCount; //Store the number of movable objects we've seen (reset to all of them for now)
+
+    //Move the objects we've seen into MoveObjectList[]
+    if (MoveObjectCount)
+        memmove(ObjectsSeen,MoveObjectList,MoveObjectCount);
+    /*for(i = 0; i < MAX_OBJECTS + 1; i++)
+    {
+        MoveObjectList[i] = ObjectsSeen[i];
+    }*/
+
+    //buv010
+    //Get the right side of the viewport and make sure it's 320
+    unsigned int iRightSide = gWinEndX;
+    if(iRightSide < 320)
+        iRightSide++;
+
+    //buv020
+    //Get the starting viewport angle
+    unsigned int iStartAngle = PlayerAngle - INT_ANGLE_32;
+    if(iStartAngle < 0) //If we've gone too far,
+        iStartAngle += INT_ANGLE_360;   //Add 360 degrees to the angle
+
+    //buv030
+    unsigned int iLeftSide = gWinStartX;
+    ViewColumn = iLeftSide;
+    iStartAngle += iLeftSide;
+    if(iStartAngle > INT_ANGLE_360)
+        iStartAngle -= INT_ANGLE_360;
+
+    //buv040
+    ViewAngle = iStartAngle;
+
+    //buv050: Main loop
+    for(i = 0; i < VIEW_WIDTH; i++)
+    {
+        //I'm not sure what I did, but somehow I ended up with angles from 0 to -32 degrees. Not good. Brute force error handling FTW!
+        if(ViewAngle < 0)
+            ViewAngle += INT_ANGLE_360;
+
+        //printf("Now in loop %d\n", i);
+        WallDistTable[i] = 4096; //init to max dist by default
+        sPtr = &(Slice[ViewColumn]);
+        //movzx edi,[_ViewAngle]
+        xRaySetup();
+        //buv060
+        yRaySetup();
+        //buv070
+        LastWallHeight = 0;
+        BuildSlice();
+
+        //multi-height wall stuff. We shouldn't really worry about this stuff, rite?
+        /*if(gMultiWalls != 0 && LastWallHeight < 200 && sPtr->Next != NULL && sPtr->Distance > 96)
+        {
+            BuildSliceMulti();
+        }*/
+
+        //buv080
+        //Increment view column and angle
+        ViewColumn++;
+        ViewAngle++;
+        //I'm not sure about this. RES_LOW causes us to increment twice? Because the assembly makes it look vice versa
+        if(Resolution == RES_LOW)
+        {
+            ViewColumn++;
+            ViewAngle++;
+            i++;
+        }
+
+        //buv090:
+        if(ViewAngle >= INT_ANGLE_360)
+            ViewAngle -= INT_ANGLE_360;
+
+        //buv100: loop again
+
+    }
+
+    //buv_exit
+    CheckHitMap();
+    FindObject();
+    FloorCeilRtn();
+    DrawWalls();
+}
+
+//CheckHitMap() - checks if movable objects are in the view. I DON'T CARE! HAHAHA
+void CheckHitMap(void)
+{
+    //For now, we'll say all the movable objects are in the view. Slower? Yes, but who cares?
+}
+
+//DrawWalls() - the workhorse for setting up all the global variables and stuff for each column of the view, so ShowCol() and
+// variants can display the walls correctly. Does very little in my version of it.
+void DrawWalls(void)
+{
+    //Store the floor height as the viewport height / 2
+    Floorht = gWinHeight / 2;
+    //and half-bitmap height
+    sctopht = ViewHeight;
+    scbotht = BITMAP_HEIGHT - sctopht;  //store height of remaining half of bitmap
+
+    //Store screen buffer center row for easy access
+    scVid = &(gScrnBuffer[gWinStartX + gCenterOff]);
+
+    //Ignore low-res code, since the game has a REALLY low resolution anyway. :P
+    //I mean, come on. Who wants to go low-res in a 320x200 game nowdays? Seriously?
+
+    //stw010: Start main loop
+    int i;
+    for(i = 0; i < VIEW_WIDTH; i++)
+    {
+        SLICE* CurSlice = &Slice[i];
+        //stw020:
+        //See if there's a slice or slices behind this one. If so, go to the farthest one first
+        while(1)
+        {
+            if(!CurSlice->Active)
+                break;
+            if(CurSlice->Next == NULL)
+                break;
+            CurSlice = CurSlice->Next;
+        }
+
+        //stw030:
+        scbNum = CurSlice->bNumber;
+        scmulcnt = scbNum & 0xFF;
+        scmulti = 0;
+        if(scbNum & WALL_TYPE_MULTI)    //Deal with a multi-height wall
+        {
+            scMulData = CurSlice->mPtr;
+            if(scMulData)
+                scmulti = 1;
+        }
+
+        bmDistance = CurSlice->Distance;
+
+        ShadingRegion = bmDistance >> 6;
+        if(ShadingRegion > 15)
+            ShadingRegion = 15;
+
+        //stw050: Light shading stuffz - Currently I have no idea what this does and I don't care
+        //ShadingRegion >>= 8; //Multiply by 256 for pallette entry
+        scColumn = CurSlice->bMap[CurSlice->bColumn];
+        scWall = *(CurSlice->bMap);    //?????????
+        scPal = &(gPalTable[ShadingRegion]);
+
+        gCurSlice = CurSlice;
+        g_iCurScreenSpot = i;
+
+        CurSlice->Fnc();    //Draw this slice
+        //Saving stuff after here? I hope it isn't important
+
+        //stw075:
+        if(CurSlice->Prev != NULL)
+        {
+            i--;
+            CurSlice = CurSlice->Prev;
+            CurSlice->Active = 0;  //MEH: don't get into infinite loop
+            continue;   //keep going
+        }
+    }
+}
+
+//Draw a wall column without light shading
+void ShowColNS(void)
+{
+    //Grab the bitmap number for this slice. And it with 0xFF to eliminate any wall flags on it (like passable, door, etc)
+    unsigned short bNumber = gCurSlice->bNumber & 0xFF;
+    //Find the spot in the wall-drawing table that we should start at
+    short* sTableSpot = LowerTable[gCurSlice->Distance];
+    //Get the height for the wall that we're supposed to draw
+    unsigned int iWallHeight = DistanceTable[gCurSlice->Distance] - 1;
+
+    if(iWallHeight > 99)
+        iWallHeight = 99;   //Force to half the screen as a max height
+    // Draw bottom half of bitmap, from the middle down
+    //CurCol will point at the screen buffer where we're supposed to draw the bitmap to. Start at the top of the buffer.
+    UCHAR* CurCol = &(gScrnBuffer[g_iCurScreenSpot]);
+    //Then move to the vertical center of the buffer
+    CurCol=&CurCol[VIEW_WIDTH * gWinHeight/2];
+    //BmpCol is the column of the bitmap that we're going to draw to the screenbuffer. Start by getting the right bitmap
+    UCHAR* BmpCol = gCurSlice->bMap[bNumber];
+    //Jump to the vertical center of the bitmap (note bitmaps are rotated 90 degrees in memory)
+    BmpCol=&BmpCol[BITMAP_HEIGHT / 2];
+    //Jump to the horizontal column we're going to draw from
+    BmpCol = &BmpCol[gCurSlice->bColumn * BITMAP_WIDTH];
+
+    //Loop through this bottom half of the bitmap
+    int i;
+    for(i = 0; i < iWallHeight; i++)
+    {
+        //The LowerTable tells us what pixels from the bitmap to draw
+        *CurCol = BmpCol[*sTableSpot];
+        //Go to next pixel in the column (320 pixels later, as screenbuffer is in row-col format)
+        CurCol=&CurCol[VIEW_WIDTH];
+        //Get next pixel number we're supposed to draw
+        sTableSpot++;
+    }
+
+    //Draw top half of bitmap, from the middle up
+    iWallHeight++; //Seems we drop a couple pixels from the top accidentally. Fix this.
+    //if(iWallHeight > 102)
+    //    iWallHeight = 102;  //Make sure we don't go above range
+    //Reset our pointers
+    sTableSpot = LowerTable[gCurSlice->Distance];
+    CurCol = &(gScrnBuffer[g_iCurScreenSpot]);
+    CurCol=&CurCol[VIEW_WIDTH * (gWinHeight/2 - 1)];
+    BmpCol = gCurSlice->bMap[bNumber];
+    BmpCol = &BmpCol[gCurSlice->bColumn * BITMAP_HEIGHT];
+    BmpCol=&BmpCol[BITMAP_HEIGHT / 2 - 1];
+    for(i = 0; i < iWallHeight; i++)
+    {
+        //Of course, now we're drawing pixels above the middle of the bitmap, so use this as a negative index
+        *CurCol = BmpCol[-(*sTableSpot)];
+        //Go up one pixel row for next pixel up in the column
+        CurCol-=VIEW_WIDTH;
+        sTableSpot++;
+    }
+
+}
+
+//Draw a wall column without light shading, replacing black with transparency
+void ShowColMaskNS(void)
+{
+    unsigned short bNumber = gCurSlice->bNumber & 0xFF;
+    short* sTableSpot = LowerTable[gCurSlice->Distance];
+    //Get the height for the wall that we're supposed to draw
+    unsigned int iWallHeight = DistanceTable[gCurSlice->Distance] - 1;
+
+    if(iWallHeight > 99)
+    {
+        iWallHeight = 99;   //Force to max height
+    }
+
+
+    // Draw bottom half of bitmap - from middle of screen down
+    UCHAR* CurCol = &(gScrnBuffer[g_iCurScreenSpot]);
+    CurCol=&CurCol[VIEW_WIDTH * gWinHeight/2];
+    UCHAR* BmpCol = gCurSlice->bMap[bNumber];
+    BmpCol=&BmpCol[BITMAP_HEIGHT / 2];
+    BmpCol = &BmpCol[gCurSlice->bColumn * BITMAP_WIDTH];
+    //CurCol += ((200 - iWallHeight) / 2) * 320;
+    int i;
+    for(i = 0; i < iWallHeight; i++)
+    {
+        //This is the same as ShowColNS(), only we don't draw this pixel if it's transparent (0, aka black)
+        if(BmpCol[*sTableSpot] != 0)
+            *CurCol = BmpCol[*sTableSpot];
+        CurCol=&CurCol[VIEW_WIDTH];
+        sTableSpot++;
+    }
+
+    //Draw top half of bitmap - from middle of screen up
+    iWallHeight++;
+    sTableSpot = LowerTable[gCurSlice->Distance];
+    CurCol = &(gScrnBuffer[g_iCurScreenSpot]);
+    CurCol=&CurCol[VIEW_WIDTH * (gWinHeight/2 - 1)];
+    BmpCol = gCurSlice->bMap[bNumber];
+    BmpCol = &BmpCol[gCurSlice->bColumn * BITMAP_HEIGHT];
+    BmpCol=&BmpCol[BITMAP_HEIGHT / 2 - 1];
+    for(i = 0; i < iWallHeight; i++)
+    {
+        if(BmpCol[-(*sTableSpot)] != 0) //Skip over this pixel if it's transparent
+            *CurCol = BmpCol[-(*sTableSpot)];
+        CurCol-=VIEW_WIDTH;
+        sTableSpot++;
+    }
+}
+
+//ShowCol - Draw a column of wall with light shading
+void ShowCol(void)
+{
+    //Grab the bitmap number for this slice. And it with 0xFF to eliminate any wall flags on it (like passable, door, etc)
+    unsigned short bNumber = gCurSlice->bNumber & 0xFF;
+    //Find the spot in the wall-drawing table that we should start at
+    short* sTableSpot = LowerTable[gCurSlice->Distance];
+    //Get the height for the wall that we're supposed to draw
+    unsigned int iWallHeight = DistanceTable[gCurSlice->Distance] - 1;
+
+    if(iWallHeight > 99)
+        iWallHeight = 99;   //Force to half the screen as a max height
+    // Draw bottom half of bitmap, from the middle down
+    //CurCol will point at the screen buffer where we're supposed to draw the bitmap to. Start at the top of the buffer.
+    UCHAR* CurCol = &(gScrnBuffer[g_iCurScreenSpot]);
+    //Then move to the vertical center of the buffer
+    CurCol=&CurCol[VIEW_WIDTH * gWinHeight/2];
+    //BmpCol is the column of the bitmap that we're going to draw to the screenbuffer. Start by getting the right bitmap
+    UCHAR* BmpCol = gCurSlice->bMap[bNumber];
+    //Jump to the vertical center of the bitmap (note bitmaps are rotated 90 degrees in memory)
+    BmpCol=&BmpCol[BITMAP_HEIGHT / 2];
+    //Jump to the horizontal column we're going to draw from
+    BmpCol = &BmpCol[gCurSlice->bColumn * BITMAP_WIDTH];
+
+    //Loop through this bottom half of the bitmap
+    int i;
+    for(i = 0; i < iWallHeight; i++)
+    {
+        *CurCol = GetLightShadedPixel(BmpCol[*sTableSpot]);
+        //Go to next pixel in the column (320 pixels later, as screenbuffer is in row-col format)
+        CurCol=&CurCol[VIEW_WIDTH];
+        //Get next pixel number we're supposed to draw
+        sTableSpot++;
+    }
+
+    //Draw top half of bitmap, from the middle up
+    iWallHeight++; //Seems we drop a couple pixels from the top accidentally. Fix this.
+    if(iWallHeight > 100)
+        iWallHeight = 100;  //Make sure we don't go above range
+    //Reset our pointers
+    sTableSpot = LowerTable[gCurSlice->Distance];
+    CurCol = &(gScrnBuffer[g_iCurScreenSpot]);
+    CurCol=&CurCol[VIEW_WIDTH * (gWinHeight/2 - 1)];
+    BmpCol = gCurSlice->bMap[bNumber];
+    BmpCol = &BmpCol[gCurSlice->bColumn * BITMAP_HEIGHT];
+    BmpCol=&BmpCol[BITMAP_HEIGHT / 2 - 1];
+    for(i = 0; i < iWallHeight; i++)
+    {
+        //Of course, now we're drawing pixels above the middle of the bitmap, so use this as a negative index
+        *CurCol = GetLightShadedPixel(BmpCol[-(*sTableSpot)]);
+        //Go up one pixel row for next pixel up in the column
+        CurCol-=VIEW_WIDTH;
+        sTableSpot++;
+    }
+}
+
+//ShowColMask - Draw a column of wall with light shading, replacing black color with transparency
+void ShowColMask(void)
+{
+    unsigned short bNumber = gCurSlice->bNumber & 0xFF;
+    short* sTableSpot = LowerTable[gCurSlice->Distance];
+    //Get the height for the wall that we're supposed to draw
+    unsigned int iWallHeight = DistanceTable[gCurSlice->Distance] - 1;
+
+    if(iWallHeight > 99)
+    {
+        iWallHeight = 99;   //Force to max height
+    }
+
+
+    // Draw bottom half of bitmap - from middle of screen down
+    UCHAR* CurCol = &(gScrnBuffer[g_iCurScreenSpot]);
+    CurCol=&CurCol[VIEW_WIDTH * gWinHeight/2];
+    UCHAR* BmpCol = gCurSlice->bMap[bNumber];
+    BmpCol=&BmpCol[BITMAP_HEIGHT / 2];
+    BmpCol = &BmpCol[gCurSlice->bColumn * BITMAP_WIDTH];
+    //CurCol += ((200 - iWallHeight) / 2) * 320;
+    int i;
+    for(i = 0; i < iWallHeight; i++)
+    {
+        //This is the same as ShowColNS(), only we don't draw this pixel if it's transparent (0, aka black)
+        if(BmpCol[*sTableSpot] != 0)
+            *CurCol = GetLightShadedPixel(BmpCol[*sTableSpot]);
+        CurCol=&CurCol[VIEW_WIDTH];
+        sTableSpot++;
+    }
+
+    //Draw top half of bitmap - from middle of screen up
+    iWallHeight++;
+    sTableSpot = LowerTable[gCurSlice->Distance];
+    CurCol = &(gScrnBuffer[g_iCurScreenSpot]);
+    CurCol=&CurCol[VIEW_WIDTH * (gWinHeight/2 - 1)];
+    BmpCol = gCurSlice->bMap[bNumber];
+    BmpCol = &BmpCol[gCurSlice->bColumn * BITMAP_HEIGHT];
+    BmpCol=&BmpCol[BITMAP_HEIGHT / 2 - 1];
+    for(i = 0; i < iWallHeight; i++)
+    {
+        if(BmpCol[-(*sTableSpot)] != 0) //Skip over this pixel if it's transparent
+            *CurCol = GetLightShadedPixel(BmpCol[-(*sTableSpot)]);
+        CurCol-=VIEW_WIDTH;
+        sTableSpot++;
+    }
+}
+
+
+//NOTE: Depends on the ranges[] array declared in ackse.cpp
+//Get the correct lightshaded value of the given pixel. Slower than his old table lookups? Quite, but I don't care, and couldn't
+//figure out what was going on with his old table lookups. "When in doubt, ignore his way of doing things" is my motto, since I'm
+//lazy. I don't exactly need the engine running at 400fps, either, like his code does on modern machines.
+unsigned char GetLightShadedPixel(unsigned char cPixel)
+{
+    //The LowerTable tells us what pixels from the bitmap to draw
+    int iColRange;
+    for(iColRange = 0; iColRange < 64; iColRange++)
+    {
+        if(ranges[iColRange].start > cPixel || ranges[iColRange].start == 0)
+            break;
+        if((ranges[iColRange].start + ranges[iColRange].length - 1) >= cPixel)
+        {
+            cPixel+=ShadingRegion;
+            if(cPixel > (ranges[iColRange].start + ranges[iColRange].length-1))
+                cPixel = ranges[iColRange].start + ranges[iColRange].length-1;
+        }
+    }
+    return cPixel;
+}
+
---
+++ ACKRTNRecode.h
@@ -0,0 +1,228 @@
+//ACK-3D Source
+//ACKRTNRecode.h - stuff needed by the routine recodes in ACKRTNRecode.c
+//Copyright (c) 2012 Mark Hutcheson
+//sort of, anyway. Lary Myers did most of the work back in 1995 or so
+#ifndef ACKRR_H
+#define ACKRR_H
+
+#include "ack3d.h"
+#include "ackext.h"
+
+void ShowCol(void);                     // Routines for drawing a slice
+void ShowColMask(void);                 // column by column
+void ShowColNS(void);
+void ShowColMaskNS(void);
+void ShowColLow(void);
+void ShowColMaskLow(void);
+
+
+//External variables needed by the functions in ACKRTNRecode.c
+extern short   gWinEndX;
+extern short   gWinStartX;
+extern long    WallDistTable[VIEW_WIDTH];
+extern UCHAR*  gScrnBuffer;
+extern short   gWinHeight;
+extern UCHAR   *scVid;         // Variables used in low level routines for
+extern UCHAR   *scWall;                // building and drawing slices
+extern UCHAR   *scPal;
+extern short   scdst;
+extern short   scwht;
+extern short   scmulti;
+extern short   sctopht;
+extern short   scbotht;
+extern short   scsavwht;
+extern short   scmulcnt;
+extern UCHAR   *scsavVid;
+extern USHORT    scbNum;
+extern UCHAR   *scMulData;
+extern UCHAR   *scColumn;
+extern UCHAR   *gPtr;
+extern UCHAR   *gmPtr;
+extern short   gBitmapNumber;
+extern short   gBitmapColumn;
+extern short   gyBitmapNumber;
+extern short   gyBitmapColumn;
+extern long    gWallDistance;
+extern short   gmPos;
+extern DOORS   *gDoor;
+extern DOORS   *gDoorPosn;
+extern short   wFound;
+extern UCHAR   *mgPtr;
+extern short   gWinFullWidth;          // Global variables for setting up a viewport
+extern long    gWinDWORDS;             // These are the global variables used by the
+extern long    gWinStartOffset;                // low-level assembly language routines to draw slices
+extern short   gWinStartX;
+extern short   gWinStartY;
+extern short   gWinEndX;
+extern short   gWinEndY;
+extern short   gWinHeight;
+extern short   gWinHalfHeight;
+extern short   gWinWidth;
+extern short   gCenterRow;
+extern short   gCenterOff;
+extern long    gBottomOff;
+extern UCHAR   *gScrnBufferCenter;
+extern UCHAR   *gScrnBuffer;
+extern UCHAR   *gBkgdBuffer;
+extern UCHAR   *gPalTable;
+extern short   gMultiWalls;
+extern short   *LowerTable[2048];
+
+//External functions needed by the functions in ACKRTNRecode.c
+extern void    (*FloorCeilRtn)(void);
+void CheckDoors(void);
+void xRaySetup(void);
+void yRaySetup(void);
+void BuildSlice(void);
+void FindObject(void);
+
+//Function prototypes for ACKRTNRecode.c functions that are only used in ACKRTNRecode.c
+void CheckHitMap(void);
+void DrawWalls(void);
+unsigned char GetLightShadedPixel(unsigned char cPixel);    //MEH Get a pixel's correct lightshaded value
+
+//MEH Variables I'm adding for ease-of-use
+SLICE* gCurSlice;
+int    g_iCurScreenSpot;//From 0 to 319
+unsigned int ShadingRegion;
+extern ColorRange  ranges[64];
+//extern ACKENG* ae;
+
+/*extern long scPtr;
+extern UCHAR *bmWall;
+
+extern long bmDistance;
+extern long BackDropRows;
+extern USHORT ScreenOffset;
+
+extern long xPglobal;
+extern long yPglobal;
+extern long xBegGlobal;
+extern long yBegGlobal;
+
+extern ACKENG *aeGlobal;
+extern USHORT *xGridGlobal;
+extern USHORT *yGridGlobal;
+extern long xPglobalHI;
+extern long yPglobalHI;
+extern ULONG *rbaTable;
+extern short rsHandle;
+
+extern long LastX1;
+extern long LastY1;
+extern long iLastX;
+extern long iLastY;
+
+extern long x_xPos;
+extern long x_yPos;
+extern long x_xNext;
+extern long x_yNext;
+extern long y_xPos;
+extern long y_yPos;
+extern long y_xNext;
+extern long y_yNext;
+
+extern short MaxDistance;
+extern short LightFlag;
+extern short ErrorCode;
+
+extern long xMapPosn;
+extern long yMapPosn;
+
+extern USHORT *Grid;
+extern USHORT *ObjGrid;
+extern SLICE Slice[VIEW_WIDTH];
+extern SLICE *sPtr;
+
+extern short TotalSpecial;
+
+extern short DistanceTable[MAX_DISTANCE + 1];
+extern long *AdjustTable;
+
+extern short xSecretmPos;
+extern short xSecretmPos1;
+extern short xSecretColumn;
+
+extern short ySecretmPos;
+extern short ySecretmPos1;
+extern short ySecretColumn;
+
+extern short TotalSecret;
+extern short ViewColumn;
+
+extern long *SinTable;
+extern long *CosTable;
+
+extern long *LongTanTable;
+extern long *LongInvTanTable;
+extern long InvCosTable[INT_ANGLE_360];
+extern long InvSinTable[INT_ANGLE_360];
+extern long *LongCosTable;
+extern long *ViewCosTable;
+
+extern long *xNextTable;
+extern long *yNextTable;
+
+extern short LastFloorAngle = -1;
+extern short LastFloorX;
+extern short LastFloorY;
+extern short LastMapPosn;
+extern short LastObjectHit;
+extern short TotalObjects;
+extern short FoundObjectCount;
+extern UCHAR ObjectsSeen[MAX_OBJECTS + 1];
+extern short MoveObjectCount;
+extern UCHAR MoveObjectList[MAX_OBJECTS + 1];
+extern UCHAR ObjNumber[MAX_OBJECTS + 1];
+extern USHORT ObjRelDist[MAX_OBJECTS + 1];
+extern short ObjColumn[MAX_OBJECTS + 1];
+extern short ObjAngle[MAX_OBJECTS + 1];
+
+extern UCHAR WorkPalette[768];
+extern UCHAR *BackArray[INT_ANGLE_360];
+extern short Resolution;
+
+extern long Flooru;
+extern long Floorv;
+extern long Floordu;
+extern long Floordv;
+extern long Floorkx;
+extern long Floorky;
+extern long Floorku;
+extern long Floorkv;
+extern long Floorkdu;
+extern long Floorkdv;
+extern UCHAR *Floorbm;
+extern UCHAR *Floorscr;
+extern UCHAR *FloorscrTop;
+extern UCHAR *Floorptr2;
+extern UCHAR *Floors1;
+extern UCHAR *Floors2;
+extern long Floorht;
+extern long Floorwt;
+extern short Floorvht;
+extern short Flooreht;
+extern long FloorLastbNum;
+extern long FloorLastbm;
+
+extern short ViewHeight = 31;
+extern short CeilingHeight = 31;
+extern short LastWallHeight;
+extern short PlayerAngle;
+extern short ViewAngle;
+extern USHORT SysFlags;
+extern UCHAR **WallbMaps;
+extern UCHAR *VidTop;
+extern UCHAR *VidBottom;
+extern short BotRowTable[320];
+extern USHORT FloorMap[4096];
+extern USHORT CeilMap[4096];
+extern UCHAR  HitMap[4096];
+
+extern UCHAR *VidSeg;
+extern char *scantables[96];
+extern UCHAR	AckKeys[128];	// Buffer for keystrokes
+extern long AckTimerCounter;*/
+
+#endif
+
